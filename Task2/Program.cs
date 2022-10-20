8eaa94c8ea3b6a8d60c9eec4cde4850325a61618
Darya <darya.novosadova@gmail.com>
2022-10-20 13:50:58 +0700

    add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число: ");
+int numbera = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int numberb = int.Parse(Console.ReadLine() ?? "0");
+int sqr1 = numbera * numbera;
+int sqr2 = numberb * numberb;
+
+if(sqr1 == numberb)
+{
+    Console.WriteLine($"Число {numberb} является квадратом числа {numbera}");
+}
+else
+{
+    Console.WriteLine($"Число {numberb} не является квадратом числа {numbera}");
+}
+if(sqr2 == numbera)
+{
+    Console.WriteLine($"Число {numbera} является квадратом числа {numberb}");
+}
+else
+{
+    Console.WriteLine($"Число {numbera} не является квадратом числа {numberb}");
+}
