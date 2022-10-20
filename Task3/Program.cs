ea01417913ec052186eba2360037bc4a7f651c79
Darya <darya.novosadova@gmail.com>
2022-10-20 14:00:42 +0700

    add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Ввидите цифру плоскости: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if(number == 1)
+{
+    Console.WriteLine("Координаты Х = 0 до +бесконечность");
+    Console.WriteLine("Координаты Y = 0 до +бесконечность");
+}
+else if (number == 2)
+{
+    Console.WriteLine("Координаты Х = 0 до -бесконечность");
+    Console.WriteLine("Координаты Y = 0 до +бесконечность");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Координаты Х = 0 до -бесконечность");
+    Console.WriteLine("Координаты Y = 0 до -бесконечность");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Координаты Х = 0 до +бесконечность");
+    Console.WriteLine("Координаты Y = 0 до -бесконечность");
+}
